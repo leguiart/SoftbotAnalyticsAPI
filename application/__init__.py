555a0929a8606be78f06269dc398744357c0af1b
leguiart <luis.eguiarte@comunidad.unam.mx>
2023-03-06 21:55:43 -0600

    .

--- application/__init__.py
+++ application/__init__.py
@@ -1765,6 +1765,6 @@ def ChooseWinnerGET(statistic):
         })
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
     
